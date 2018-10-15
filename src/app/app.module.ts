c78650c014c7c765b26056b992426632988da188
Ucok Freddy <ucokfm@gmail.com>
2018-10-15 16:41:34 +0700

    Create basic route for Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+];
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
     MaterialModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
   bootstrap: [AppComponent],
