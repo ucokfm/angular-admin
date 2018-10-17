9b24d02f2e40c584131bb521cc1febf91e869367
Ucok Freddy <ucokfm@gmail.com>
2018-10-17 20:30:55 +0700

    Drawer mode and opened based on responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MediaChange, ObservableMedia } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-admin';
+export class AppComponent implements OnDestroy {
+  drawerMode = 'side';
+  drawerOpened = true;
+  title = 'Angular Admin';
+  watcher: Subscription;
+
+  constructor(private media: ObservableMedia) {
+    this.watcher = media.subscribe((change: MediaChange) => {
+      if (change.mqAlias === 'xs') {
+        this.drawerMode = 'over';
+        this.drawerOpened = false;
+      } else if (change.mqAlias === 'sm') {
+        this.drawerMode = 'side';
+        this.drawerOpened = false;
+      } else if (change.mqAlias === 'md') {
+        this.drawerMode = 'side';
+        this.drawerOpened = true;
+      } else if (change.mqAlias === 'lg') {
+        this.drawerMode = 'side';
+        this.drawerOpened = true;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.watcher.unsubscribe();
+  }
 }
