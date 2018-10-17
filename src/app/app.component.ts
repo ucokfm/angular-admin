fce85e895f8f53d17788bbc43bddcee94468aded
Ucok Freddy <ucokfm@gmail.com>
2018-10-17 20:57:34 +0700

    Create onSelectMenu handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MediaChange, ObservableMedia } from '@angular/flex-layout';
+import { MatDrawer } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,10 @@ export class AppComponent implements OnDestroy {
   title = 'Angular Admin';
   watcher: Subscription;
 
-  constructor(private media: ObservableMedia) {
+  @ViewChild('drawer')
+  drawer: MatDrawer;
+
+  constructor(private media: ObservableMedia, private router: Router) {
     this.watcher = media.subscribe((change: MediaChange) => {
       if (change.mqAlias === 'xs') {
         this.drawerMode = 'over';
@@ -33,5 +38,12 @@ export class AppComponent implements OnDestroy {
 
   ngOnDestroy() {
     this.watcher.unsubscribe();
+  }
+
+  onSelectMenu(path: string) {
+    if (this.drawerMode === 'over') {
+      this.drawer.toggle();
+    }
+    this.router.navigate([path]);
   }
 }
