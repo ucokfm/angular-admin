c7fde06d3d61b9fa7adf2cba87e9a45d75db4230
Ucok Freddy <ucokfm@gmail.com>
2018-10-22 16:22:51 +0700

    Use zone.run to trigger rerender from product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
+import { Injectable, NgZone } from '@angular/core';
+import { Subject } from 'rxjs';
 import * as io from 'socket.io-client';
 import feathers, { Paginated, Service } from '@feathersjs/feathers';
 import socketio from '@feathersjs/socketio-client';
@@ -21,32 +21,35 @@ export interface Product {
   providedIn: 'root',
 })
 export class ProductService {
-  products$: Observable<Product[]>;
-  private observer: Observer<Product[]>;
+  zone: NgZone;
+  products$: Subject<Product[]>;
   private products: Product[];
   private service: Service<Product>;
 
   constructor() {
-    this.products$ = new Observable(observer => {
-      this.observer = observer;
-    });
+    this.zone = new NgZone({ enableLongStackTrace: false });
+    this.products$ = new Subject();
     this.products = [];
     this.service = client.service('products');
     this.service.on('created', product => {
-      this.products.push(product);
-      this.observer.next([...this.products]);
+      this.zone.run(() => {
+        this.products.push(product);
+        this.products$.next([...this.products]);
+      });
     });
     this.service.on('updated', product => {
-      const index = this.products.findIndex(item => item._id === product._id);
-      this.products[index] = product;
-      this.observer.next([...this.products]);
+      this.zone.run(() => {
+        const index = this.products.findIndex(item => item._id === product._id);
+        this.products[index] = product;
+        this.products$.next([...this.products]);
+      });
     });
   }
 
   find() {
     this.service.find().then((result: Paginated<Product>) => {
       this.products = result.data;
-      this.observer.next([...result.data]);
+      this.products$.next([...result.data]);
     });
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,31 +1,16 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
-import { Product, ProductService } from '../product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent implements OnDestroy, OnInit {
-  products: Array<Product>;
-  subscription: Subscription;
-
-  constructor(
-    private productService: ProductService,
-    private ref: ChangeDetectorRef
-  ) {}
+export class ProductsComponent implements OnInit {
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.subscription = this.productService.products$.subscribe(products => {
-      this.products = products;
-      this.ref.detectChanges();
-    });
     this.productService.find();
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 }
